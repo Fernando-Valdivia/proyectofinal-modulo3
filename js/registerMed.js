fd1b73bbd1f3d985528859a579baf277e7a8b53e
Nacho Capra <mugammas2@gmail.com>
2024-05-15 16:18:48 -0300

    Reacomodamiento Textos y botones

---
+++ js/registerMed.js
@@ -0,0 +1,6 @@
+//Logica para registrar medicos.
+
+/*EJEMPLO:
+
+En la validacion,si el medico ingresa las credenciales correctas, en el condicional  ingresamos window.location.href = '../pages/dashboard_med.html; para que redireccione a el html donde va a estar todo lo que ve el medico 
+*/
---
+++ js/registerPac.js
@@ -0,0 +1,8 @@
+//Logica para registrar Pacientes
+
+/*EJEMPLO:
+
+En la validacion,si el medico ingresa las credenciales correctas, en el condicional 
+ ingresamos window.location.href = '../pages/dashboard_pac.html; 
+ para que redireccione a el html donde va a estar todo lo que ve el paciente
+*/
