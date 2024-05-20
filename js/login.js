b7acb539a28b28ccf0148cff9beceebc67d52cca
Fabrizio <fabrigabrielcoronel@gmail.com>
2024-05-20 16:03:39 -0300

    cambie detalles en el js

--- js/login.js
+++ js/login.js
@@ -1,86 +1,72 @@
-const validarLoginPacientes = document.querySelector("#validarLoginPacientes")
-validarLoginPacientes.addEventListener("submit", validarPaciente)
-
-
+const validarLoginPacientes = document.querySelector("#validarLoginPacientes");
+validarLoginPacientes.addEventListener("submit", validarPaciente);
 
 function validarPaciente(e) {
   e.preventDefault();
-
 
   const id = Date.now();
   const emailPaciente = document.querySelector("#emailPacientes").value;
   const passwordPaciente = document.querySelector("#passwordPacientes").value;
 
-  const validarEmailPaciente = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ ; 
+  const validarEmailPaciente = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
   const resultadoValidacionPaciente = validarEmailPaciente.test(emailPaciente);
- 
-   if(!resultadoValidacionPaciente)
+
+  if (!resultadoValidacionPaciente)
     return Swal.fire({
       icon: "error",
       title: "Ingrese un email valido!",
-      text: "Vuelva a intentarlo"
-      
+      text: "Vuelva a intentarlo",
     });
- 
 
-
-  if(emailPaciente === "" || passwordPaciente === ""){
-    
+  if (emailPaciente === "" || passwordPaciente === "") {
     return Swal.fire({
       icon: "error",
       title: "Debes llenar todos los campos!",
-      text: "Vuelva a intentarlo"
-      
+      text: "Vuelva a intentarlo",
     });
-
-
   }
 
   Swal.fire({
     title: "Iniciaste sesion!",
-    icon: "success"
+    icon: "success",
+    position: "center",
+    showConfirmButton: false,
+    timer: 1500,
   });
-
 }
 
-const validarLoginMedicos = document.querySelector("#validarLoginMedicos")
-validarLoginMedicos.addEventListener("submit", validarMedico)
-
-
+const validarLoginMedicos = document.querySelector("#validarLoginMedicos");
+validarLoginMedicos.addEventListener("submit", validarMedico);
 
 function validarMedico(e) {
   e.preventDefault();
-
 
   const id = Date.now();
   const emailMedico = document.querySelector("#emailMedicos").value;
   const passwordMedico = document.querySelector("#passwordMedicos").value;
 
-  const validarEmailMedico = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ ;
+  const validarEmailMedico = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
   const resultadoValidacionMedico = validarEmailMedico.test(emailMedico);
-  if(!resultadoValidacionMedico)
+  if (!resultadoValidacionMedico)
     return Swal.fire({
       icon: "error",
       title: "Ingrese un email valido!",
-      text: "Vuelva a intentarlo"
-      
+      text: "Vuelva a intentarlo",
     });
-    
-  if(emailMedico === "" || passwordMedico === ""){
-    
+
+  if (emailMedico === "" || passwordMedico === "") {
     return Swal.fire({
       icon: "error",
       title: "Debes llenar todos los campos!",
-      text: "Vuelva a intentarlo"
-      
+      text: "Vuelva a intentarlo",
     });
-
-
   }
 
   Swal.fire({
     title: "Iniciaste sesion!",
-    icon: "success"
+    icon: "success",
+    position: "center",
+    showConfirmButton: false,
+    timer: 1500,
   });
-
-} 
+}
